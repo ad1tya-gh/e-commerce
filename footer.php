e0e65bf2202a9518dad8a396a74f62796114d4d3
ad1tya-gh <ahmd.aditya13@gmail.com>
2024-10-30 08:54:03 +0800

    add footer-

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!-- footer -->
+
+    <div class="container-fluid py-5 content-subscribe text-light">
+        <div class="container">
+            <h5 class="text-center mb-4">Temui Kami</h5>
+            <div class="row justify-content-center">
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <i></i>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <i></i>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <i></i>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center">
+                    <i></i>
+                </div>
+            </div>
+
+            <h5 class="text-center mt-5 mb-5">Subscribe</h5>
+            <p class="text-center">untuk informasi</p>
+        </div>
+    </div>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    require "koneksi.php";
+    $queryProduk = mysqli_query($conn, "SELECT id, nama, harga, foto, detail FROM produk LIMIT 6");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,24 +39,65 @@
             <h3>Kategori Terlaris</h3>
 
             <div class="row mt-5">
-                <div class="col-4">
+                <div class="col-md-4 mb-3">
                     <div class="highlighted-kategori kategori-baju-pria d-flex justify-content-center align-items-center">
-                        <h4 class="text-white"><a href="produk.php?kategori=Pakaian Pria">Baju Pria</a></h4>
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=Pakaian Pria">Baju Pria</a></h4>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-md-4 mb-3">
                     <div class="highlighted-kategori kategori-sepatu d-flex justify-content-center align-items-center">
-                        <h4 class="text-white"><a href="produk.php?kategori=Sepatu">Sepatu</a></h4>
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=Sepatu">Sepatu</a></h4>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-md-4 mb-3">
                     <div class="highlighted-kategori kategori-baju-wanita d-flex justify-content-center align-items-center">
-                        <h4 class="text-white"><a href="produk.php?kategori=Pakaian Wanita">Baju Wanita</a></h4>
+                        <h4 class="text-white"><a class="no-decoration"href="produk.php?kategori=Pakaian Wanita">Baju Wanita</a></h4>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- tentang kami -->
+
+    <div class="container-fluid warna3 py-5">
+        <div class="container text-center">
+            <h3>Tentang Kami</h3>
+            <p class="fs-6 mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laudantium facilis aspernatur incidunt atque esse molestias. Corrupti perspiciatis iusto provident reprehenderit! Provident, beatae aspernatur repellat, dolorem similique, quasi praesentium error repellendus fugiat non nam excepturi? Nam repudiandae animi at quisquam rem tenetur amet, quis a fuga? Ullam deserunt, similique voluptates et odio possimus corporis animi eum veniam nemo adipisci aliquid beatae consequuntur a ipsam obcaecati expedita tenetur facere? Architecto molestias similique suscipit harum voluptate illum quo at aliquid porro doloremque.</p>
+        </div>
+    </div>
+
+    <!-- produk -->
+
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3>Produk</h3>
+
+            <div class="row mt-5">
+                <?php while($data = mysqli_fetch_array($queryProduk)){ ?>
+                <div class="col-sm-6 col-md-4 mb-3">
+                    <div class="card h-100">
+                        <div class="image-box">
+                        <img src="image/<?php echo $data['foto']; ?>" class="card-img-top" alt="...">
+
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php echo $data['nama']; ?></h4>
+                            <p class="card-text text-truncate"><?php echo $data['detail']; ?></p>
+                            <p class="text-harga card-text">Rp.<?php echo $data['harga']; ?></p>
+                            <a href="produk-detail.php?q=<?php echo $data['nama']; ?> " class="btn warna2 text-white">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+
+                
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+     <?php require "footer.php"; ?>
 
 
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
